4bec5aeda37e26b134e1ae8ee31e87a6d0f697fe
Mélissa Loeser <melissa.loeser@gmail.com>
2019-06-04 11:43:57 +0200

    update auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -57,7 +57,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     # Uncomment the section below if you want users to be created if they don't exist
     unless user
-         user = User.create(name: data['name'],
+         user = User.create(last_name: data['name'],
             email: data['email'],
             password: Devise.friendly_token[0,20]
          )
