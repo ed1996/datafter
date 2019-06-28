ded9321c2907d4dc5f08165ee3c3a0792a65bfb5
Mélissa Loeser <melissa.loeser@gmail.com>
2019-06-28 18:17:00 +0200

    add not resize ckeditor

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -17,6 +17,7 @@ CKEDITOR.editorConfig = function( config ) {
 
   config.colorButton_colors = 'ad5a55,eeebe4,1f2236,c8c8c8,000','ceb078';
   config.colorButton_enableMore = false;
+  config.resize_enabled = false;
 
   config.removeButtons = 'BGColor,Save,Templates,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,HiddenField,ImageButton,Subscript,Superscript,CopyFormatting,RemoveFormat,NumberedList,Outdent,Indent,CreateDiv,JustifyLeft,JustifyCenter,JustifyRight,JustifyBlock,BidiLtr,BidiRtl,Language,Link,Unlink,Anchor,Image,Flash,Table,HorizontalRule,PageBreak,Iframe,ShowBlocks,Maximize,About,Preview,Print,Source,FontSize,Styles,Font';
 };
