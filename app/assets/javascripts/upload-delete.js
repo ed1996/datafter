54b661e2f55b6def676ed63d777c831f0685f760
Mélissa Loeser <melissa.loeser@gmail.com>
2019-06-30 13:08:21 +0200

    update upload image hommage

--- app/assets/javascripts/upload-delete.js
+++ app/assets/javascripts/upload-delete.js
@@ -26,52 +26,70 @@ function deletePicture (e, url, method) {
       });
     }
   } else {
-    removePicture(e);
+    if ( confirm( "Voulez-vous vraiment supprimer cette image ?" ) ) {
+      removePicture(e);
+      toastr.info('La photo a bien été supprimé');
+    }
   }
 }
 let pictures = [];
 
-function readURL(input, uniq) {
+function readURL (picture,uniq) {
+  let reader = new FileReader();
+
+  let nbImg = pictures.length || 0;
+  reader.onload = function (e) {
+    let colMd = document.createElement("div");
+    let preview = document.createElement("div");
+    let img = document.createElement("img");
+    let t = document.createTextNode("Suppresion");
+
+    colMd.setAttribute('class','col-md-4 photo-not-send');
+    colMd.setAttribute('id',uniq ? 'photo' : 'photo-' + 'notApi' + nbImg);
+    preview.setAttribute('class','panel-heading preview');
+    img.setAttribute('src',e.target.result);
+
+    preview.appendChild(img);
+    colMd.appendChild(preview);
+    if (!uniq) {
+      let button = document.createElement("button");
+      button.setAttribute('class','btn-width-full btn btn-transparent-2 btn-xxs');
+      button.setAttribute('type','button');
+      button.setAttribute('data-picture', 'notApi' + nbImg);
+      button.setAttribute('value',e.target.result);
+
+      button.addEventListener("click", uniq ? (function(){ removePicture(this, uniq); }) : deletePicture, false);
+      button.appendChild(t);
+      colMd.appendChild(button);
+    }
+
+    document.getElementById("photos").appendChild(colMd);
+  };
+  reader.readAsDataURL(picture);
+  pictures.push(picture);
+}
+
+function removeElementsByClass(className){
+  let elements = document.getElementsByClassName(className);
+  while(elements.length > 0){
+    elements[0].parentNode.removeChild(elements[0]);
+  }
+}
+
+function addPicture(input, uniq) {
   if (uniq && document.getElementById('photo')) {
     document.getElementById('photo').remove();
   }
-  if (input.files && input.files[0]) {
-      let reader = new FileReader();
 
-      let nbImg = pictures.length || 0;
-      reader.onload = function (e) {
-        let colMd = document.createElement("div");
-        let preview = document.createElement("div");
-        let img = document.createElement("img");
-        var inputImg = document.createElement("input");
-        var t = document.createTextNode("Suppresion");
-
-        colMd.setAttribute('class','col-md-4');
-        colMd.setAttribute('id',uniq ? 'photo' : 'photo-' + 'notApi' + nbImg);
-        preview.setAttribute('class','panel-heading preview');
-        img.setAttribute('src',e.target.result);
-        inputImg.setAttribute('type','file');
-        inputImg.setAttribute('style','display: none;');
-        inputImg.setAttribute('name',`images[${input.files[0]}]`);
-
-        preview.appendChild(img);
-        colMd.appendChild(preview);
-        if (!uniq) {
-          let button = document.createElement("button");
-          button.setAttribute('class','btn-width-full btn btn-transparent-2 btn-xxs');
-          button.setAttribute('type','button');
-          button.setAttribute('data-picture', 'notApi' + nbImg);
-          button.setAttribute('value',e.target.result);
-
-          button.addEventListener("click", uniq ? (function(){ removePicture(this, uniq); }) : deletePicture, false);
-          button.appendChild(t);
-          colMd.appendChild(button);
-        }
-        colMd.appendChild(inputImg);
-        document.getElementById("photos").appendChild(colMd);
-      };
-
-      reader.readAsDataURL(input.files[0]);
-      pictures.push(input.files[0]);
+  if (input.files && input.files.length) {
+    if (input.multiple) {
+      removeElementsByClass('photo-not-send');
+      for (let i = 0; i < input.files.length; i++) {
+        readURL(input.files[i], uniq);
+      }
+    }
+    else {
+      readURL(input.files[0], uniq);
+    }
   }
 }
